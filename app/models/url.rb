856061f89380d2176b59d2b1ca50a72b7459f51c
chunsiangl <cslim_324@hotmail.com>
2017-10-17 16:33:11 +0800

    added strong params and delete

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
 class Url < ApplicationRecord
+	validates :long_url, presence: true
+	validates :long_url, uniqueness: {case_sensitive: false, message: "was shortened before"}
+	validates :long_url, format: {with: (URI::regexp(['http', 'https'])), message: "format is not good"}
 end
